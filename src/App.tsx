8d6d67708e306361c144782b10759d9c6d7b8e6b
Priyankaadev <priyaaswal79@gmail.com>
2026-06-04 16:48:31 +0530

    added grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Grid from "./components/Grid"
 import { PathfindingProvider } from "./context/PathfindingContext"
 import { SpeedProvider } from "./context/SpeedContext"
 import { TileProvider } from "./context/TileContext"
@@ -9,8 +10,9 @@ function App() {
 <PathfindingProvider>
   <TileProvider>
     <SpeedProvider>
-    <h1 className="text-red-500 text-2xl h-screen w-screen bg-yellow-100 font-bold">
-      Hello</h1>
+    <div className="h-screen w-screen flex flex-col">
+      <Grid />
+      </div>
       </SpeedProvider>
       </TileProvider>
   </PathfindingProvider>
