565cd83dbcbd760b35e24383b20456e8a91228b7
Priyankaadev <priyaaswal79@gmail.com>
2026-06-04 14:43:29 +0530

    done: speed context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { PathfindingProvider } from "./context/PathfindingContext"
+import { SpeedProvider } from "./context/SpeedContext"
 import { TileProvider } from "./context/TileContext"
 
 
@@ -7,8 +8,10 @@ function App() {
   return (
 <PathfindingProvider>
   <TileProvider>
+    <SpeedProvider>
     <h1 className="text-red-500 text-2xl h-screen w-screen bg-yellow-100 font-bold">
       Hello</h1>
+      </SpeedProvider>
       </TileProvider>
   </PathfindingProvider>
   )
