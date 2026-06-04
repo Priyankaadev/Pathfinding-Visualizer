ad6d438cab9f21765ed9e561b72fcf4351da6cfc
Priyankaadev <priyaaswal79@gmail.com>
2026-06-04 12:11:01 +0530

    added: pathfinding context and utils

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+  <div>
+    <h1 className="text-red-500 text-2xl h-screen w-screen bg-yellow-100 font-bold">
+      Hello</h1>
+  </div>
+  )
+}
+
+export default App
---
+++ src/utils/helpers.ts
@@ -0,0 +1,5 @@
+import type { GridType, TileType } from "./types";
+
+export const createGrid = (startTile: TileType, endTile: TileType) =>{
+    const grid: GridType = [];
+}
