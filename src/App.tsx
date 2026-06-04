64eb7e7c818d2a3f46834d130529be3a08fcd153
Priyankaadev <priyaaswal79@gmail.com>
2026-06-04 14:25:12 +0530

    added: tile context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { PathfindingProvider } from "./context/PathfindingContext"
+import { TileProvider } from "./context/TileContext"
 
 
 function App() {
 
   return (
 <PathfindingProvider>
+  <TileProvider>
     <h1 className="text-red-500 text-2xl h-screen w-screen bg-yellow-100 font-bold">
       Hello</h1>
+      </TileProvider>
   </PathfindingProvider>
   )
 }
